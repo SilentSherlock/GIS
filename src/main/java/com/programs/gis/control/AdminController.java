dbb30208018739dc9fb4619f76ce28bd0b442748
SilentSherlock <1536141515@qq.com>
2020-02-27 18:56:40 +0800

    add admin service

--- src/main/java/com/programs/gis/control/AdminController.java
+++ src/main/java/com/programs/gis/control/AdminController.java
@@ -1,8 +1,23 @@
 package com.programs.gis.control;
 
+import com.programs.gis.entity.Admin;
+import com.programs.gis.service.AdminService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AdminController {
+    @Resource
+    private AdminService adminService;
 
+    @RequestMapping("/adminLogin")
+    public String adminLogin(HttpServletRequest request){
+        Admin admin = new Admin();
+        request.setAttribute("adminLogin", admin);
+        return "adminLogin";
+    }
 }
--- src/main/java/com/programs/gis/service/AdminService.java
+++ src/main/java/com/programs/gis/service/AdminService.java
@@ -13,6 +13,7 @@ public class AdminService {
     private AdminDao adminDao;
 
     public Admin adminValidate(String adminName, String password) throws Exception{
+        System.out.println("Admin Validate");
         List<Admin> admins = adminDao.getAdminsByName(adminName);
         if (admins != null){
             for (Admin tmp:admins) {
@@ -23,5 +24,26 @@ public class AdminService {
         }
         System.out.println("AdminService:Get nothing with this adminName");
         return null;
+    }
+    public List<Admin> getAllAdmins() throws Exception{
+        System.out.println("Get All Admins");
+        return adminDao.getAllAdmins();
+    }
+    public Admin getAdminByNameAndPass(String adminName, String password) throws Exception{
+        System.out.println("Get Admin By Name And Password");
+        return adminDao.getAdminByNameAndPass(adminName, password);
+    }
+    public void addAdmin(String adminName, String password, String mail) throws Exception{
+        System.out.println("Add New Admin");
+        Admin admin = new Admin();
+        admin.setAdminName(adminName);
+        admin.setPassword(password);
+        admin.setMail(mail);
+        adminDao.save(admin);
+    }
+    public void deleteAdmin(String adminName, String password) throws Exception{
+        System.out.println("Delete Admin");
+        Admin admin = adminDao.getAdminByNameAndPass(adminName,password);
+        adminDao.deleteById(admin.getAdminId());
     }
 }
